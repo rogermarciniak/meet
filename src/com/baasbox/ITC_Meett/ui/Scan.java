a1c4f2fc590d8f674fd0ad898a3d82196a08edd3
rogermarciniak <roger@yatsu.eu>
2016-05-08 21:01:14 +0100

    scan progress

--- src/com/baasbox/ITC_Meett/ui/Scan.java
+++ src/com/baasbox/ITC_Meett/ui/Scan.java
@@ -1,6 +1,7 @@
 package com.baasbox.ITC_Meett.ui;
 
 import android.content.Intent;
+import android.location.Location;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -16,20 +17,17 @@ import com.baasbox.android.BaasResult;
 import com.baasbox.android.BaasUser;
 
 import com.baasbox.android.json.JsonObject;
-import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.api.GoogleApiClient;
-import com.google.android.gms.common.api.GoogleApiClient.ConnectionCallbacks;
-import com.google.android.gms.common.api.GoogleApiClient.OnConnectionFailedListener;
-import com.google.android.gms.location.LocationListener;
-import com.google.android.gms.location.LocationRequest;
 import com.google.android.gms.location.LocationServices;
-
-import com.baasbox.ITC_Meett.R;
-
-
 import java.util.List;
 
+
 public class Scan extends AppCompatActivity implements GoogleApiClient.ConnectionCallbacks{
+
+    private GoogleApiClient mGoogleApiClient;
+    private Location mLastLocation;
+    private String mLatitudeText;
+    private String mLongitudeText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,8 +35,7 @@ public class Scan extends AppCompatActivity implements GoogleApiClient.Connectio
         setContentView(R.layout.activity_scan);
 
         // Create an instance of GoogleAPIClient.
-
-        GoogleApiClient mGoogleApiClient = new GoogleApiClient.Builder(this)
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
                 .addConnectionCallbacks(this)
                 .addOnConnectionFailedListener(this)
                 .addApi(LocationServices.API)
@@ -69,6 +66,10 @@ public class Scan extends AppCompatActivity implements GoogleApiClient.Connectio
             mLatitudeText.setText(String.valueOf(mLastLocation.getLatitude()));
             mLongitudeText.setText(String.valueOf(mLastLocation.getLongitude()));
         }
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
     }
 
     protected void onStop() {
