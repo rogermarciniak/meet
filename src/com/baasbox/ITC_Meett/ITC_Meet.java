632839af23b5a6cd661810a50dbea763b7973a3a
rogermarciniak <roger@yatsu.eu>
2016-05-15 23:22:15 +0100

    scan progress

--- src/com/baasbox/ITC_Meett/ITC_Meet.java
+++ src/com/baasbox/ITC_Meett/ITC_Meet.java
@@ -14,17 +14,12 @@ public class ITC_Meet extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
-        //todo 1.2
+
         BaasBox.builder(this).setAuthentication(BaasBox.Config.AuthType.SESSION_TOKEN)
                 .setApiDomain("vps.yatsu.eu")
                 .setPort(9000)
                 .setAppCode("1234567890")
                 .init();
-//        BaasBox.Config config = new BaasBox.Config();
-//        config.authenticationType= BaasBox.Config.AuthType.SESSION_TOKEN;
-//        config.apiDomain = "192.168.56.1"; // the host address
-//        config.httpPort=9000;
-//        BaasBox.initDefault(this,config);
     }
 
 }
